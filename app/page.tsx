a621b49763d779a914350e7183398c171a4f62c0
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-16 10:54:32 +0530

    [feat]: add camp section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Camp from "@/components/Camp";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-   <Hero/>
+    <>
+      <Hero />
+      <Camp />
+    </>
   );
 }

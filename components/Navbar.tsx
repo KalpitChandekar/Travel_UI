836a0519dbe0807cb533ca38e7273f6c7f05247a
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-17 12:09:51 +0530

    update navbar menu section for small screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,29 @@
+'use client'
+
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 import { NAV_LINKS } from "@/constants";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul
+        className={`hidden lg:flex h-full gap-12 ${
+          menuOpen ? "flex" : "hidden"
+        }`}
+      >
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
@@ -22,7 +35,7 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="lg:flexCenter hidden">
+      <div className="hidden lg:flexCenter">
         <Button
           type="button"
           title="Login"
@@ -32,13 +45,38 @@ const Navbar = () => {
       </div>
 
       <Image
-        src="menu.svg"
+        src="/menu.svg"
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={handleMenuToggle}
       />
+
+      {/* Mobile Menu */}
+      {menuOpen && (
+        <ul className="absolute top-16 right-0 mt-2 w-full bg-white flex flex-col items-center gap-6 py-4 lg:hidden">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 cursor-pointer transition-all hover:font-bold"
+              onClick={handleMenuToggle} // Close menu on link click
+            >
+              {link.label}
+            </Link>
+          ))}
+          <Button
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+            onClick={handleMenuToggle} // Close menu on button click
+          />
+        </ul>
+      )}
     </nav>
   );
 };
+
 export default Navbar;
